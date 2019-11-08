e569edc41117307bf178357aba55a85db0e21f02
senaps <gerdakan.sa@gmail.com>
2019-11-08 16:51:15 +0330

    copy template files to the project path

--- sfgen/cli.py
+++ sfgen/cli.py
@@ -2,13 +2,33 @@ import argparse
 import os
 import sys
 
+from distutils.dir_util import copy_tree
+
+TEMPLATES_PATH = os.path.join(os.path.abspath("."), "templates/")
+
+
+def make_dest(project_path, app_name, app_type):
+    """check if the path is okay to write to
+
+    we will check if the path doesnt already exist, and then create the app
+    project structure, copying all the files from the templates to it.
+    """
+    tmp_path = os.path.join(project_path, app_name)
+    if os.path.exists(tmp_path):
+        raise FileExistsError(f"a file in {tmp_path} already exists.")
+    os.mkdir(tmp_path)  # create the project path
+    copy_tree(TEMPLATES_PATH + app_type, tmp_path)
+    return True
+
 
 def make_simple_app(app_name, app_path):
-    pass
+    """
+    """
+    make_dest(app_name=app_name, project_path=app_path, app_type="simple_app")
 
 
 def make_full_app(app_name, app_path):
-    pass
+    make_dest(app_name=app_name, project_path=app_path, app_type="full_app")
 
 
 APP_TYPES = {
@@ -26,4 +46,4 @@ if __name__ == "__main__":
     parser.add_argument("-p", "--path", default=".", dest="project_path",
                         help="the path to create the app in")
     args = parser.parse_args()
-    APP_TYPES[args.app_type](name, project_path)
+    APP_TYPES[args.app_type](args.name, args.project_path)
