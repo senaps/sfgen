7b95e070fa842592408370b92f2e527c0d451798
senaps <gerdakan.sa@gmail.com>
2019-12-19 20:40:47 +0330

    fixed the setup

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
+from __version__ import version
+
+description = 'generate an application, as fast as possible'
 
 setup(
     name='sfgen',
-    version='1.0.0',
+    version=version,
     packages=find_packages(),
     url='http://www.senaps.blog.ir',
     license='MIT',
     author='sneaps',
     author_email='gerdakan.sa@gmail.com',
-    description='generate an application, as fast as possible',
-    install_requires=[
-            'click',
-            'requests',
-        ],
+    description=description,
+        #TODO: entry points should be fixed with new style:))
     entry_points={
         'console_scripts': [
-            'sfgen = sfgen.cli:cli'
+            'sfgen = sfgen.cli:main'
         ],
     }
 )
